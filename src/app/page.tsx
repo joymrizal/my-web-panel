19c5187c647b3f07723351bbab58349df9f48317
joymrizal <joymrizal@gmail.com>
2025-04-13 20:27:09 +0700

    20.27

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,9 @@ export default function LandingPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-4 text-center px-4">
       {/* Teks Landing Page */}
-      <h1 className="text-3xl font-bold mb-4 text-gray-800">Selamat Datang di Website Kami</h1>
-      <p className="text-lg mb-8 text-gray-600">Platform yang mempermudah semua kebutuhan Anda. Daftar atau login untuk mulai.</p>
+      <h1 className="text-3xl font-bold mb-4 text-gray-800">Selamat Datang di Website Joy MR IOT</h1>
+      <p className="text-lg mb-8 text-gray-600">Platform yang mempermudah semua kebutuhan Anda. Daftar atau login untuk mulai</p>
+      <p className="text-lg mb-8 text-gray-600">Koneksi online dengan PLC</p>
 
       {/* Tombol Login dan Register */}
       <div className="flex gap-4 justify-center">
